ece1989c16e75d0f8766298761ca9d1849762037
rivindups <rivindupiyumal50@gmail.com>
2025-02-21 12:49:18 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,7 +20,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~XSAykL4Y#Kii8tWc_vnTKc6PPzuW7BvqHGQEGxxsOJwPY6Hpdoxs",
 BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
 PASSWORD: process.env.PASSWORD || "Enter you like password",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
